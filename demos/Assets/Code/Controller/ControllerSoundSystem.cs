dc6f1521c76f3c8d658ba1c5b86f9c6d26fa360e
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-05-24 13:26:33 -0400

    added more settings to the sound system

--- demos/Assets/Code/Controller/ControllerSoundSystem.cs
+++ demos/Assets/Code/Controller/ControllerSoundSystem.cs
@@ -15,9 +15,25 @@ namespace Goldenwere.Unity.Controller
         [SerializeField]    private MaterialCollection[]    clipsMaterials;
         [Tooltip            ("Associate audio clips with terrain textures here. These must match the textures applied to the terrain.")]
         [SerializeField]    private AudioClip[]             clipsTerrain;
+        [Tooltip            ("The pitch to use while crouched (ideally lower than the pitch while not crouched)")]
+        [SerializeField]    private float                   settingPitchWhileCrouched = 0.5f;
+        [Tooltip            ("The pitch to use while not crouched (ideally 1)")]
+        [SerializeField]    private float                   settingPitchWhileNotCrouched = 1f;
+        [Tooltip            ("The volume to use while crouched (ideally lower than the pitch while not crouched)")]
+        [SerializeField]    private float                   settingVolumeWhileCrouched = 0.5f;
+        [Tooltip            ("The volume to use while not crouched (ideally 1)")]
+        [SerializeField]    private float                   settingVolumeWhileNotCrouched = 1f;
         [Tooltip            ("Any GameObject with the tags defined here will play audio. " +
                             "Do not tag terrain with a tag that is listed here, or else the default audio will play (due to terrain-checking using a different method)")]
         [SerializeField]    private string[]                tagObjects;
+        [Tooltip            ("The time between playing footsteps while moving crouched")]
+        [SerializeField]    private float                   timeBetweenStepsCrouch = 1f;
+        [Tooltip            ("The time between playing footsteps while moving fast")]
+        [SerializeField]    private float                   timeBetweenStepsFast = 0.3333f;
+        [Tooltip            ("The time between playing footsteps while moving normally")]
+        [SerializeField]    private float                   timeBetweenStepsNorm = 0.5f;
+        [Tooltip            ("The time between playing footsteps while moving slow")]
+        [SerializeField]    private float                   timeBetweenStepsSlow = 1f;
 
         private Dictionary<Material, AudioClip> workingMaterials;
         private AudioSource workingSource;
@@ -38,8 +54,10 @@ namespace Goldenwere.Unity.Controller
     [Serializable]
     public struct MaterialCollection
     {
+#pragma warning disable 0649
         [SerializeField]    private AudioClip   associatedClip;
         [SerializeField]    private Material[]  associatedMaterials;
+#pragma warning restore 0649
 
         public Material[]   AssociatedMaterials { get { return associatedMaterials; } }
         public AudioClip    AssociatedClip      { get { return associatedClip; } }

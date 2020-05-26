8a7013fe963293de2652b08a3526b746fee7c75f
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-05-26 12:53:39 -0400

    subscribed to and handled UpdateMovementState and outlined the entry of the audio loop

--- demos/Assets/Code/Controller/ControllerSoundSystem.cs
+++ demos/Assets/Code/Controller/ControllerSoundSystem.cs
@@ -5,38 +5,43 @@ using UnityEngine;
 namespace Goldenwere.Unity.Controller
 {
     [RequireComponent(typeof(AudioSource))]
+    [RequireComponent(typeof(FirstPersonController))]
     public class ControllerSoundSystem : MonoBehaviour
     {
         #region Fields
 #pragma warning disable 0649
         [Tooltip            ("The default clip to play when moving and no other material/texture was found. At minimum, if using this system, this should be defined.")]
-        [SerializeField]    private AudioClip               clipDefaultMovement;
+        [SerializeField]    private AudioClip                       clipDefaultMovement;
         [Tooltip            ("Associate audio clips with any materials you want to have specific sounds for while stepping on them.")]
-        [SerializeField]    private MaterialCollection[]    clipsMaterials;
+        [SerializeField]    private MaterialCollection[]            clipsMaterials;
         [Tooltip            ("Associate audio clips with terrain textures here. These must match the textures applied to the terrain.")]
-        [SerializeField]    private AudioClip[]             clipsTerrain;
+        [SerializeField]    private AudioClip[]                     clipsTerrain;
         [Tooltip            ("The pitch to use while crouched (ideally lower than the pitch while not crouched)")]
-        [SerializeField]    private float                   settingPitchWhileCrouched = 0.5f;
+        [SerializeField]    private float                           settingPitchWhileCrouched = 0.5f;
         [Tooltip            ("The pitch to use while not crouched (ideally 1)")]
-        [SerializeField]    private float                   settingPitchWhileNotCrouched = 1f;
+        [SerializeField]    private float                           settingPitchWhileNotCrouched = 1f;
         [Tooltip            ("The volume to use while crouched (ideally lower than the pitch while not crouched)")]
-        [SerializeField]    private float                   settingVolumeWhileCrouched = 0.5f;
+        [SerializeField]    private float                           settingVolumeWhileCrouched = 0.5f;
         [Tooltip            ("The volume to use while not crouched (ideally 1)")]
-        [SerializeField]    private float                   settingVolumeWhileNotCrouched = 1f;
+        [SerializeField]    private float                           settingVolumeWhileNotCrouched = 1f;
         [Tooltip            ("Any GameObject with the tags defined here will play audio. " +
                             "Do not tag terrain with a tag that is listed here, or else the default audio will play (due to terrain-checking using a different method)")]
-        [SerializeField]    private string[]                tagObjects;
+        [SerializeField]    private string[]                        tagObjects;
         [Tooltip            ("The time between playing footsteps while moving crouched")]
-        [SerializeField]    private float                   timeBetweenStepsCrouch = 1f;
+        [SerializeField]    private float                           timeBetweenStepsCrouched = 1f;
         [Tooltip            ("The time between playing footsteps while moving fast")]
-        [SerializeField]    private float                   timeBetweenStepsFast = 0.3333f;
+        [SerializeField]    private float                           timeBetweenStepsFast = 0.3333f;
         [Tooltip            ("The time between playing footsteps while moving normally")]
-        [SerializeField]    private float                   timeBetweenStepsNorm = 0.5f;
+        [SerializeField]    private float                           timeBetweenStepsNorm = 0.5f;
         [Tooltip            ("The time between playing footsteps while moving slow")]
-        [SerializeField]    private float                   timeBetweenStepsSlow = 1f;
-
-        private Dictionary<Material, AudioClip> workingMaterials;
-        private AudioSource workingSource;
+        [SerializeField]    private float                           timeBetweenStepsSlow = 1f;
+        
+        /**************/    private FirstPersonController           attachedController;
+        /**************/    private MovementState                   workingCurrentMovementState;
+        /**************/    private float                           workingCurrentStepTime;
+        /**************/    private Dictionary<Material, AudioClip> workingMaterials;
+        /**************/    private AudioSource                     workingSource;
+        /**************/    private float                           workingTimeSinceLastPlayed;
 #pragma warning restore 0649
         #endregion
 
@@ -50,8 +55,60 @@ namespace Goldenwere.Unity.Controller
                         workingMaterials.Add(m, mc.AssociatedClip);
 
             workingSource = GetComponent<AudioSource>();
+            attachedController = GetComponent<FirstPersonController>();
+            workingCurrentStepTime = timeBetweenStepsNorm;
         }
-    }
+
+        private void OnEnable()
+        {
+            attachedController.UpdateMovementState += OnUpdateMovementState;
+        }
+
+        private void OnDisable()
+        {
+            attachedController.UpdateMovementState -= OnUpdateMovementState;
+        }
+
+        private void Update()
+        {
+            if (workingCurrentMovementState != MovementState.idle && workingCurrentMovementState != MovementState.idle_crouched &&
+                workingCurrentMovementState != MovementState.jumping && workingCurrentMovementState != MovementState.falling)
+            {
+                if (workingTimeSinceLastPlayed >= workingCurrentStepTime)
+                {
+
+                }
+            }
+        }
+
+        private void OnUpdateMovementState(MovementState state)
+        {
+            switch (state)
+            {
+                case MovementState.fast:
+                    workingCurrentStepTime = timeBetweenStepsFast;
+                    workingSource.pitch = settingPitchWhileNotCrouched;
+                    break;
+                case MovementState.slow:
+                    workingCurrentStepTime = timeBetweenStepsSlow;
+                    workingSource.pitch = settingPitchWhileNotCrouched;
+                    break;
+                case MovementState.fast_crouched:
+                case MovementState.norm_crouched:
+                case MovementState.slow_crouched:
+                    workingCurrentStepTime = timeBetweenStepsCrouched;
+                    workingSource.pitch = settingPitchWhileCrouched;
+                    break;
+                case MovementState.norm:
+                    workingCurrentStepTime = timeBetweenStepsNorm;
+                    workingSource.pitch = settingPitchWhileNotCrouched;
+                    break;
+                default:
+                    // Do nothing for the other states - sound doesn't play
+                    break;
+            }
+        }
+}
 
     [Serializable]
     public struct MaterialCollection

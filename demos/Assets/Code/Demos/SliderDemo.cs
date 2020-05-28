c7859a2e05d41beb668c8bf8e10577b59f427cdc
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-05-28 15:30:53 -0400

    created functioning demo for sliders

--- demos/Assets/Code/Demos/SliderDemo.cs
+++ demos/Assets/Code/Demos/SliderDemo.cs
@@ -1,18 +1,43 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Goldenwere.Unity.UI;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Collections;
 
-public class SliderDemo : MonoBehaviour
+namespace Goldenwere.Unity.Demos
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SliderDemo : MonoBehaviour
     {
-        
-    }
+        [SerializeField]    private SliderTextLoadExtension                         textLoadDemoSlider;
+        [SerializeField]    private Slider                                          transitionDemoSlider;
+        /**************/    private bool                                            transitionCoroutineIsRunning;
+        /**************/    private Dictionary<Slider, SliderTransitionExtension>   transitionExtensions;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            textLoadDemoSlider.UpdateText(3f);
+            textLoadDemoSlider.AssociatedSlider.SetValueWithoutNotify(3f);
+            transitionExtensions = new Dictionary<Slider, SliderTransitionExtension>()
+            {
+                { transitionDemoSlider, new SliderTransitionExtension(this, transitionDemoSlider, 0.5f, 0.75f) }
+            };
+        }
+
+        private void Update()
+        {
+            if (!transitionCoroutineIsRunning)
+                StartCoroutine(PeriodicallyUpdateSlider());
+
+            foreach (SliderTransitionExtension s in transitionExtensions.Values)
+                s.Update();
+        }
+
+        private IEnumerator PeriodicallyUpdateSlider()
+        {
+            transitionCoroutineIsRunning = true;
+            transitionExtensions[transitionDemoSlider].UpdateValue(Random.Range(0, 500));
+            yield return new WaitForSeconds(Random.Range(0.25f, 3f));
+            transitionCoroutineIsRunning = false;
+        }
     }
 }

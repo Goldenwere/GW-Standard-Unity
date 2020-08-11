7224f61c78d5ad3d8ab7cfc2ee81a30da8153935
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-08-11 15:24:57 -0400

    add new demo code

---
+++ demos/Assets/Code/Demos/ManagementCamDemo.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using Goldenwere.Unity.Controller;
+
+namespace Goldenwere.Unity.Demos
+{
+    /// <summary>
+    /// Class for managing the GodGameCam demo
+    /// </summary>
+    public class ManagementCamDemo : MonoBehaviour
+    {
+        #region Fields
+#pragma warning disable 0649
+        [SerializeField] private ManagementCamera cam;
+#pragma warning restore 0649
+        #endregion
+        #region Methods
+        /// <summary>
+        /// On Start, enable cam
+        /// </summary>
+        private void Start()
+        {
+            cam.controlMotionEnabled = true;
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+
+        /// <summary>
+        /// Subscribe to events on Enable
+        /// </summary>
+        private void OnEnable()
+        {
+            cam.CameraMouseStateChanged += OnCameraMouseStateChanged;
+        }
+
+        /// <summary>
+        /// Unsubscribe from events on Disable
+        /// </summary>
+        private void OnDisable()
+        {
+            cam.CameraMouseStateChanged -= OnCameraMouseStateChanged;
+        }
+
+        /// <summary>
+        /// Handler for the CameraMouseStateChanged event which toggles cursor state
+        /// </summary>
+        /// <param name="isMouseBeingUsed">Whether the mouse is currently being used for camera motion</param>
+        private void OnCameraMouseStateChanged(bool isMouseBeingUsed)
+        {
+            if (isMouseBeingUsed)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+        #endregion
+    }
+}

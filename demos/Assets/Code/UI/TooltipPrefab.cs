c1279aeb9f9228b9c1d89ec20c61d89e8c5865fd
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-08-13 13:01:27 -0400

    created class that holds info for the tooltip element

---
+++ demos/Assets/Code/UI/TooltipPrefab.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Goldenwere.Unity.UI
+{
+    /// <summary>
+    /// Contains references to the attached elements for the tooltip prefab used by TooltipEnabledElement
+    /// </summary>
+    public class TooltipPrefab : MonoBehaviour
+    {
+#pragma warning disable 0649
+        [Header("Elements")]
+        [Tooltip         ("Element that points to the element that the tooltip is associated with")]
+        [SerializeField] private Image      arrow;
+        [Tooltip         ("Whether to enable the usage of an arrow with this prefab")]
+        [SerializeField] private bool       arrowEnabled;
+        [Tooltip         ("The text that gets updated")]
+        [SerializeField] private TMP_Text   text;
+#pragma warning restore 0649
+
+        public Image            Arrow           { get { return arrow; } }
+        public bool             ArrowEnabled    { get { return arrowEnabled; } }
+        public RectTransform    RTransform      { get { return GetComponent<RectTransform>(); } }
+        public TMP_Text         Text            { get { return text; } }
+    }
+}

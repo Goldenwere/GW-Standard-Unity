81fa54aade42319f81094692d905d0999866ce8d
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-08-13 13:07:36 -0400

    attached tooltip prefab class to prefab

--- demos/Assets/Code/UI/TooltipPrefab.cs
+++ demos/Assets/Code/UI/TooltipPrefab.cs
@@ -12,15 +12,18 @@ namespace Goldenwere.Unity.UI
 #pragma warning disable 0649
         [Header("Elements")]
         [Tooltip         ("Element that points to the element that the tooltip is associated with")]
-        [SerializeField] private Image      arrow;
+        [SerializeField] private Image          arrow;
         [Tooltip         ("Whether to enable the usage of an arrow with this prefab")]
-        [SerializeField] private bool       arrowEnabled;
+        [SerializeField] private bool           arrowEnabled;
+        [Tooltip         ("Canvas group for animation opacity")]
+        [SerializeField] private CanvasGroup    canvasGroup;
         [Tooltip         ("The text that gets updated")]
-        [SerializeField] private TMP_Text   text;
+        [SerializeField] private TMP_Text       text;
 #pragma warning restore 0649
 
         public Image            Arrow           { get { return arrow; } }
         public bool             ArrowEnabled    { get { return arrowEnabled; } }
+        public CanvasGroup      CGroup     { get { return canvasGroup; } }
         public RectTransform    RTransform      { get { return GetComponent<RectTransform>(); } }
         public TMP_Text         Text            { get { return text; } }
     }

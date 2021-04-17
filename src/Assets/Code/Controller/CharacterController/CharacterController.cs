0c8a7db8b5417ffbbe55251c7a343724a49081fb
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2021-04-17 00:11:08 -0400

    change how controller initialization is handled

--- src/Assets/Code/Controller/CharacterController/CharacterController.cs
+++ src/Assets/Code/Controller/CharacterController/CharacterController.cs
@@ -3,17 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Goldenwere.Unity.Controller
-{ 
+{
+    public delegate void ControllerLoadedDelegate(CharacterController loaded);
+
     public partial class CharacterController : MonoBehaviour
     {
 #pragma warning disable 0649
+        [Tooltip                                            ("(Default: true) Automatically initializes the controller on Start; can be disabled for manually calling Initialize")]
+        [SerializeField] private bool                       initializeOnStart = true;
         [SerializeField] private InputSettings              settingsForInput;
 #pragma warning restore
+        /**************/ private bool                       initialized;
         /**************/ private ControllerInputs           inputs;
+        /**************/ public ControllerLoadedDelegate    controllerLoaded;
+        
+        public void Initialize()
+        {
+            if (!initialized)
+            { 
+                if (settingsForInput.playerInput == null)
+                    throw new System.NullReferenceException("[gw-std-unity] Controller's PlayerInput is null; " +
+                        "this must be assigned in order for the controller to work properly. Either assign one in inspector or do so in code before the first frame.");
+                else
+                    InitializeInput();
+                
+                initialized = true;
+                controllerLoaded?.Invoke(this);
+            }
+        }
 
         private void Start()
         {
-            StartCoroutine(LateStart());
+            if (initializeOnStart)
+                Initialize();
         }
 
         private void Update()
@@ -24,17 +46,6 @@ namespace Goldenwere.Unity.Controller
         private void FixedUpdate()
         {
         
-        }
-
-        private IEnumerator LateStart()
-        {
-            yield return new WaitForEndOfFrame();
-
-            if (settingsForInput.playerInput == null)
-                throw new System.NullReferenceException("[gw-std-unity] Controller's PlayerInput is null; " +
-                    "this must be assigned in order for the controller to work properly. Either assign one in inspector or do so in code before the first frame.");
-            else
-                InitializeInput();
         }
     }
 }
